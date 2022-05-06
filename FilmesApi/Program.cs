1ddbc376e059a5ce0065e65bddb088ded39969fd
Rodrigo Sandeski <sandeski47@gmail.com>
2022-05-06 01:08:47 -0300

    ConnectionString and dbcontext

--- FilmesApi/Program.cs
+++ FilmesApi/Program.cs
@@ -1,4 +1,12 @@
+using FilmesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<FilmeContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FilmeConnection"));
+});
 
 // Add services to the container.
 
@@ -8,7 +16,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
