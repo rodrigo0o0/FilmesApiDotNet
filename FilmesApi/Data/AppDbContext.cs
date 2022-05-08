c6023e204c751cc8fc2c64b86cff9a062869670d
Rodrigo Sandeski <sandeski47@gmail.com>
2022-05-08 05:17:36 -0300

    Criada a entidadde Endereco e Profile de endereco

--- FilmesApi/Data/AppDbContext.cs
+++ FilmesApi/Data/AppDbContext.cs
@@ -11,5 +11,7 @@ namespace FilmesApi.Data
         }
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
     }
 }
---
+++ FilmesApi/Profiles/EnderecoProfile.cs
@@ -0,0 +1,17 @@
+﻿using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using AutoMapper;
+
+
+namespace FilmesApi.Profiles
+{
+    public class EnderecoProfile : Profile
+    {
+
+        public EnderecoProfile(){
+            CreateMap<CreateEnderecoDto, Endereco>();
+            CreateMap<Endereco, ReadEnderecoDto>();
+            CreateMap<UpdateEnderecoDto, Endereco>();
+        }
+    }
+}
